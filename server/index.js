10c4e0379f6fc525c2659eb4aa123872cb158574
Lior Shub <liorbentov@gmail.com>
2018-06-18 09:36:59 +0300

    express

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
+const app = express();
+
+const compiler = webpack(webpackConfig);
+
+console.log(path.resolve(__dirname, '../dist'));
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+app.use(webpackDevMiddleware(compiler, {
+  hot: true,
+  filename: 'bundle.js',
+  publicPath: '/',
+  stats: {
+    colors: true,
+  },
+  historyApiFallback: true,
+}));
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+});
+
+const server = app.listen(3000, function() {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
