17e3a1f31d8f49f6bb02b1295b6842ff0a266d70
Lior Shub <liorbentov@gmail.com>
2018-06-22 17:04:22 +0300

    minor style

--- client/player/index.js
+++ client/player/index.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Gravatar from "react-gravatar";
+import classNames from 'classnames';
+
+import Styles from './style.css';
 
 class Player extends Component {
   render() {
     const { name, mail } = this.props;
     return (
-      <div>
-        <Gravatar email={mail || name} />
+      <div className={classNames(Styles['player-container'])}>
+        <Gravatar email={mail || name} size={100} className={classNames(Styles.gravatar)} />
+        <span className="player-name">{name}</span>
       </div>
     );
   }
